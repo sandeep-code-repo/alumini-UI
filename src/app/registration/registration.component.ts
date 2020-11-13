09a78f22e82fb48a233a0e5f1fa76afa87cc13e5
priyambada_20 <priyambada@nucigent.in>
2020-11-13 14:54:18 +0530

    validationaddition

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterModule, Routes, Router, RouterLink } from '@angular/router';
-import { Validators, FormBuilder, FormGroup, FormArray, FormControl } from '@angular/forms';
+import { Validators, FormBuilder, FormGroup, FormArray, FormControl, FormArrayName } from '@angular/forms';
 
 import { routerTransition } from '../services/config/config.service';
 
@@ -10,7 +10,7 @@ import { PlantInfoData } from 'src/app/plant-info-data';
 import { HttpClient } from '@angular/common/http';
 
 import { CustomValidators } from 'src/app/custom-validators';
-import { Industry, ParameterInfo, StationInfo, StationInfoMapper, UserInfo,Role } from '../model/industry.model';
+import { Industry, ParameterInfo, StationInfo, StationInfoMapper, UserInfo, Role } from '../model/industry.model';
 
 import * as $ from "jquery";
 
@@ -32,16 +32,17 @@ export class RegistrationComponent implements OnInit {
   plant: PlantInfoData;
   stationInfo: StationInfo;
   submitted: Boolean = false;
- stationinfomap: StationInfoMapper[]=[]
+  stationinfomap: StationInfoMapper[] = []
   userInfo: UserInfo;
   message: any;
   addProcesssubmitted: boolean;
-role:Role;
-userRole:Role[]=[]
+
+  role: Role;
+  userRole: Role[] = []
   types$: { Category: string; }[];
-stackDisplay:Boolean=false
+  stackDisplay: Boolean = false
   constructor(private route: Router, private userService: UserService, private formBuilder: FormBuilder, private HttpClient: HttpClient) {
-this.role={roleId:0}
+    this.role = { roleId: 0 }
   }
 
 
@@ -49,69 +50,73 @@ this.role={roleId:0}
   ngOnInit(): void {
 
     this.processForm = this.formBuilder.group({
-      stationInfo:this.formBuilder.group({
-      analyzerv2: ['', [Validators.required]],
-      stationId: ['', [Validators.required]],
-      location: ['', [Validators.required]],
-      stationVendor: ['', [Validators.required]],
-     
-      longitute: ['', [Validators.required]],
-      latitude: ['', [Validators.required]],
-      measurementPrinciple: ['', [Validators.required]],
-      stackHeight: ['', [Validators.required]],
-      stackDiameter: ['', [Validators.required]],
-      stackVelocity: ['', [Validators.required]],
-      gasDischargeRate: ['', [Validators.required]],
-      remarks: ['', [Validators.required]],
+      stationInfo: this.formBuilder.group({
+        analyzerv2: ['', [Validators.required]],
+        stationId: ['', [Validators.required]],
+        location: ['', [Validators.required]],
+        stationVendor: ['', [Validators.required]],
 
-    }),
-    parameterInfo: this.formBuilder.array([this.createParameterInfo()],[Validators.required]),
-  })
-    // this.parameterInfo.push(this.addParameter());
-    // this.parameterinfo.push(this.createParameterInfo().value)
-    
+        longitute: ['', [Validators.required]],
+        latitude: ['', [Validators.required]],
+        measurementPrinciple: ['', [Validators.required]],
+        stackHeight: ['', [Validators.required]],
+        stackDiameter: ['', [Validators.required]],
+        stackVelocity: ['', [Validators.required]],
+        gasDischargeRate: ['', [Validators.required]],
+        remarks: ['', [Validators.required]],
+
+      }),
+      parameterInfo: this.formBuilder.array([this.createParameterInfo()], [Validators.required]),
+    })
+    //this.parameterInfo.push(this.addParameter());
+    //this.parameterinfo.push(this.createParameterInfo().value)
+
     this.stationinfomap.push(this.processForm.value)
-    
+
     this.add_industry = this.formBuilder.group({
-      plantInfo:this.formBuilder.group({
-        plantVendor:[''],
+      plantInfo: this.formBuilder.group({
+        plantVendor: ['', [Validators.required]],
         plantUserName: ['', [Validators.required]],
         category: ['', [Validators.required]],
         plantName: ['', [Validators.required]],
-        plantType:[],
+        plantType: [],
         roUser: ['', [Validators.required]],
-        caaqmsStation: ['', [Validators.required]],
-        cemsStation: ['', [Validators.required]],
-        ceqmsStation: ['', [Validators.required]],
-        
-        
+        caaqmsStation: ['', [Validators.pattern(/^[0-9]\d*$/)]],
+        cemsStation: ['', [Validators.pattern(/^[0-9]\d*$/)]],
+        ceqmsStation: ['', [Validators.pattern(/^[0-9]\d*$/)]],
+
+
       }),
-     
-     // role: ['',],
-    userInfoMapper:this.formBuilder.group({
-   userInfo:this.formBuilder.group({
-    userName: ["", [Validators.required]],
-    password:['',[Validators.required]],
-    roleId:[''],
-    pin: ['', [Validators.required, Validators.minLength(7), CustomValidators.patternValidator(/\d/, { hasNumber: true })]],
-    typ:[''],
-    district: ['', [Validators.required]],
-    town: ['', [Validators.required]],
-    street: ['', [Validators.required]],
-    state: ['', [Validators.required]],
-    email: ['', [Validators.required, Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')]],
-    authPerson: ['', [Validators.required]],
-    mobNo: ['', [Validators.required, Validators.minLength(10), CustomValidators.patternValidator(/\d/, { hasNumber: true })]],
-    authPersonDesig: ['', [Validators.required]],
-    secdPerson: ['', [Validators.required]],
-    secdPersonDesig: ['', [Validators.required]],
-    secdPersonMob: ['', [Validators.required, Validators.minLength(10), CustomValidators.patternValidator(/\d/, { hasNumber: true })]],
-    secdEmail: ['', [Validators.required, Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')]],
-   }),
+
+      // role: ['',],
+      userInfoMapper: this.formBuilder.group({
+        userInfo: this.formBuilder.group({
+          userName: ["", [Validators.required]],
+          selectpassword: ['', Validators.required],
+          password: ['', [Validators.required, Validators.pattern('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&].{8,}')]],
+          roleId: [''],
+          selectpin: ['', Validators.required],
+          pin: ['', [Validators.required, Validators.pattern("[0-9]{6}")]],
+          typ: [''],
+          district: ['', [Validators.required]],
+          town: ['', [Validators.required]],
+          street: ['', [Validators.required]],
+          state: ['', [Validators.required]],
+          email: ['', [Validators.required, Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')]],
+          authPerson: ['', [Validators.required]],
+          //selectmobNo: ['', Validators.required],
+          mobNo: ['', [Validators.required, Validators.pattern("[0-9]{10}")]],
+          authPersonDesig: ['', [Validators.required]],
+          secdPerson: ['', [Validators.required]],
+          secdPersonDesig: ['', [Validators.required]],
+          //selectsecdPersonMob: ['', Validators.required],
+          secdPersonMob: ['', [Validators.pattern("[0-9]{10}")]],
+          secdEmail: ['', [Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')]],
+        }),
 
 
-    })
-  });
+      })
+    });
 
 
   }
@@ -144,16 +149,16 @@ this.role={roleId:0}
     })
 
   }
-  
+
 
   editProcess(editProcessObj) {
-   
+
     this.processForm.patchValue({
-     
-      stationInfo:editProcessObj.stationInfo,
+
+      stationInfo: editProcessObj.stationInfo,
       parameterInfo: editProcessObj.parameterInfo,
-     
-     
+
+
     })
   }
 
@@ -162,21 +167,25 @@ this.role={roleId:0}
     this.route.navigateByUrl("/regdetails");
   }
   get f() { return this.add_industry.controls; }
+  get plantInfof() { return (this.add_industry.get('plantInfo') as FormGroup).controls }
+  get userInfof() { return (this.add_industry.get('userInfoMapper.userInfo') as FormGroup).controls }
+
 
   get processf() { return this.processForm.controls }
- 
-  addProcess() {
- 
-// console.log(this.processForm.get('parameterInfo'))
-    
-//     this.addProcesssubmitted=true;
-//     if (this.processForm.invalid) {
+  get stationinfoV() { return (this.processForm.get('stationInfo') as FormGroup).controls }
+  // get parameterinfoV(){return (this.processForm.get('i') as FormGroup).control}
 
-//          return;
-//        }
-// if(this.processForm.get('stationInfo.analyzerv2').value=='Emission'){this.stackDisplay=true;}
-// else
-// this.stackDisplay=false;
+  addProcess() {
+
+    this.addProcesssubmitted = true;
+    if (this.processForm.invalid) {
+
+               return;
+             }
+    //    
+    // if(this.processForm.get('stationInfo.analyzerv2').value=='Emission'){this.stackDisplay=true;}
+    // else
+    // this.stackDisplay=false;
     let index = this.stationinfomap.findIndex(item => {
       return item.stationInfo.stationId == this.processForm.get('stationInfo.stationId').value;
     });
@@ -190,33 +199,34 @@ this.role={roleId:0}
     else if (index > -1) {
 
       this.stationinfomap.splice(index, 1, this.processForm.value)
-      
+
     } else {
       this.stationinfomap.push(this.processForm.value);
     }
     this.processForm.reset();
   }
   register() {
-   // this.submitted=true;
-   
-    // if (this.add_industry.invalid) {
+    
+    this.submitted = true;
 
-    //   return;
-    // }
+    if (this.add_industry.invalid) {
+
+      return;
+    }
 
 
     this.industry = this.add_industry.value
-    if(this.add_industry.get('userInfoMapper.userInfo').status=='INVALID')
-    this.industry.regstatus = "register"
-    else 
-    this.industry.regstatus = "register"
+    if (this.add_industry.get('userInfoMapper.userInfo').status == 'INVALID')
+      this.industry.regstatus = "register"
+    else
+      this.industry.regstatus = "register"
     this.industry.stationInfoMapper = this.stationinfomap;
-    this.role.roleId=this.add_industry.get('userInfoMapper.userInfo.roleId').value
+    this.role.roleId = this.add_industry.get('userInfoMapper.userInfo.roleId').value
     this.userRole.push(this.role)
     Object.assign(this.industry.userInfoMapper, { userRole: this.userRole })
-    
+
     console.log(this.industry);
-    
+
     const register = this.userService.registrationService(this.industry).subscribe(data => {
 
 
@@ -225,7 +235,7 @@ this.role={roleId:0}
         this.message = "Registration Successfull"
         console.log(data.apiStatus.message);
         // this.route.navigate(['/home']);
-
+        this.add_industry.reset();
       } else {
         this.message = "Registration Failed"
         console.log(data);
@@ -233,48 +243,47 @@ this.role={roleId:0}
 
     });
   }
-  stack(str:string)
-  {
-    
-   
-    if(str=="Ambient")
-    {
+  stack(str: string) {
+
+
+    if (str == "Ambient") {
       //this.stackDisplay=false;
-      
-      $('#stack_123').css('display','none');
-      this.types$=[{"Category":"PM10"},{"Category":"PM2.5"},
-      {"Category":"SOx"},
-      {"Category":"NOx"},{"Category":"NO"},{"Category":"NO2"},{"Category":"CO"},{"Category":"CO2"},{"Category":"Others"
+
+      $('#stack_123').css('display', 'none');
+      this.types$ = [{ "Category": "PM10" }, { "Category": "PM2.5" },
+      { "Category": "SOx" },
+      { "Category": "NOx" }, { "Category": "NO" }, { "Category": "NO2" }, { "Category": "CO" }, { "Category": "CO2" }, {
+        "Category": "Others"
       }];
     }
-   else
-    if(str=="Emission")
-    {
-    // this.stackDisplay=true;
-      this.addProcesssubmitted=true;
-      // $('.stackDisplay').css('display','block');
-      $('#stack_123').css('display','block');
+    else
+      if (str == "Emission") {
+        // this.stackDisplay=true;
+        this.addProcesssubmitted = true;
+        // $('.stackDisplay').css('display','block');
+        $('#stack_123').css('display', 'block');
 
-      this.types$=[{"Category":"PM"},{"Category":"SO2"},
-      {"Category":"NOx"},
-      {"Category":"NO"},{"Category":"NO2"},{"Category":"SOx"},{"Category":"HF"},{"Category":"Others"
-      }];
-    }
-else
-    if(str=="Effluent")
-    {
-      
-      //this.stackDisplay=false;
-      $('#stack_123').css('display','none');
-      this.types$=[{"Category":"BOD"},{"Category":"COD"},
-      {"Category":"TSS"},
-      {"Category":"PH"},{"Category":"Flow"},{"Category":"Fluoride"},{"Category":"Cr6+"},{"Category":"NH3"},{"Category":"Others"
-      }];
-    }
+        this.types$ = [{ "Category": "PM" }, { "Category": "SO2" },
+        { "Category": "NOx" },
+        { "Category": "NO" }, { "Category": "NO2" }, { "Category": "SOx" }, { "Category": "HF" }, {
+          "Category": "Others"
+        }];
+      }
+      else
+        if (str == "Effluent") {
+
+          //this.stackDisplay=false;
+          $('#stack_123').css('display', 'none');
+          this.types$ = [{ "Category": "BOD" }, { "Category": "COD" },
+          { "Category": "TSS" },
+          { "Category": "PH" }, { "Category": "Flow" }, { "Category": "Fluoride" }, { "Category": "Cr6+" }, { "Category": "NH3" }, {
+            "Category": "Others"
+          }];
+        }
 
 
 
-    
+
   }
-  
+
 }
