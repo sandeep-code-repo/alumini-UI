970b631f32ded9fb69ceeaf040aa4cd5f4fda625
priyambada_20 <priyambada@nucigent.in>
2020-12-01 20:35:03 +0530

    clear

--- src/app/pipes/filter-parent.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { FilterParentPipe } from './filter-parent.pipe';
-
-describe('FilterParentPipe', () => {
-  it('create an instance', () => {
-    const pipe = new FilterParentPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
--- src/app/services/grid.module.ts
+++
@@ -1,5 +0,0 @@
-export class DynamicGrid{     
-    name:string;  
-    email:string;  
-    phone:string;  
-}
